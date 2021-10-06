0e4ef6659eea3422dd85f7dbe33c34f96d724980
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2021-10-05 22:58:22 -0300

    Add project files.

---
+++ AWSS3Uploader/Program.cs
@@ -0,0 +1,47 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using System;
+using System.IO;
+
+namespace AWSS3Uploader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IAmazonS3 s3Client;
+
+            RegionEndpoint regionEndpoint = RegionEndpoint.USWest2;
+
+            s3Client = new AmazonS3Client(regionEndpoint);
+
+            var transfer = new TransferUtility(s3Client);
+
+            var fileName = $"{Guid.NewGuid().ToString()}.rec";
+            using (var file = File.Open($"{fileName}", FileMode.OpenOrCreate))
+            {
+                using (StreamWriter writer = new StreamWriter(file))
+                {
+                    writer.WriteLine("nome;cpf;valor;data");
+
+                    for (int i = 0; i < 100; i++)
+                    {
+                        writer.WriteLine($"CodersInRio{i};{i.ToString().PadLeft(11, '0')};{new Random().Next(1, 10000).ToString("N2")};{DateTime.Now.AddHours(i)}");
+                    }
+
+                    writer.Flush();
+                }
+            }
+
+
+            var fileUpload = File.Open($"{fileName}", FileMode.OpenOrCreate);
+
+            transfer.UploadAsync(fileUpload, bucketName: "codersinriobucket", key: fileName).Wait();
+
+            Console.WriteLine("Upload realizado com sucesso");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MVCS3/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVCS3.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCS3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile s3Form)
+        {
+            
+            RegionEndpoint regionEndpoint = RegionEndpoint.USWest2;
+
+            IAmazonS3 s3Client = new AmazonS3Client(regionEndpoint);
+
+            var transfer = new TransferUtility(s3Client);
+
+            MemoryStream ms = new MemoryStream();
+
+            var uploadFile = s3Form.OpenReadStream();
+
+            await uploadFile.CopyToAsync(ms);
+
+            var fileName = s3Form.FileName;
+
+            await transfer.UploadAsync(ms, "codersinriobucket", fileName);
+
+            return View();
+        }
+
+      
+    }
+}
